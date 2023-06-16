2a069a95d078436db527823ae38cbd86a40cbeeb
BengtRydberg <bengt.rydberg@smhi.se>
2023-06-16 15:50:00 +0200

    adding filter option

--- pps_mw_training/evaluation.py
+++ pps_mw_training/evaluation.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from tensorflow import keras  # type: ignore
 from xarray import Dataset  # type: ignore
 import matplotlib.pyplot as plt  # type: ignore
@@ -27,18 +28,26 @@ def evaluate_quantile_performance(
             print(f"{param} quantile {quantile}: {obtained_quantile}")
 
 
-def get_stats(bins, true_state: np.ndarray, predicted_state: np.ndarray):
+def get_stats(
+    bins,
+    true_state: np.ndarray,
+    predicted_state: np.ndarray,
+    extra_filter: Optional[np.ndarray] = None,
+):
     retrived_median = np.zeros(bins.size - 1)
     for idx in range(bins.size - 1):
         filt = (true_state >= bins[idx]) & (true_state < bins[idx + 1])
+        if extra_filter is not None:
+            filt = filt & extra_filter
         retrived_median[idx] = np.median(predicted_state[filt])
     return (bins[0:-1] + bins[1::]) / 2., retrived_median
 
 
 def plot_prediction(
-    true_state: np.ndarray,
-    predicted_state: np.ndarray,
-    n_edges: int = 200,
+    true_state: Dataset,
+    predicted_state: Dataset,
+    n_edges: int = 100,
+    min_iwp: float = 0.01,
 ) -> None:
     """Plot prediction and the known state."""
     plt.figure(figsize=(12, 10))
@@ -67,11 +76,19 @@ def plot_prediction(
             value_range = np.array([min_value, max_value])
             plt.plot(value_range, value_range, "-k", label="1-to-1")
             bins = np.linspace(min_value, max_value, n_edges)
+            if param in ["Dmean", "Zmean"]:
+                extra_filter = (
+                    (true_state["IWP"].values >= min_iwp)
+                    & (predicted_state["IWP"][:, idx_q].values >= min_iwp)
+                )
+            else:
+                extra_filter = None
             for idx in [-1, 0, 1]:
                 center, q = get_stats(
                     bins,
                     true_state[param].values * scale,
                     predicted_state[param][:, idx_q + idx].values * scale,
+                    extra_filter=extra_filter,
                 )
                 plt.plot(center, q, "-", label=f"Q{idx + 2}")
         plt.xlim(value_range)
