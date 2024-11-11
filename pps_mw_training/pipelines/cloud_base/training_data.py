ea0c58b6cd5187a130ffc7dfc966eed7eee23899
sm_indka <inderpreet.kaur@smhi.se>
2024-11-11 15:36:19 +0100

    removed hardcoded stuff, moved func update_std_mean to utils, and allow training_label_name passing as input

--- pps_mw_training/pipelines/cloud_base/training_data.py
+++ pps_mw_training/pipelines/cloud_base/training_data.py
@@ -1,16 +1,13 @@
 from pathlib import Path
-from typing import Any, Optional
-import datetime as dt
+from typing import Any, Tuple
 import json
-import re
 import random
-import pickle
 import numpy as np  # type: ignore
 import tensorflow as tf  # type: ignore
 import xarray as xr  # type: ignore
-from scipy.ndimage import rotate
-
+from scipy.ndimage import rotate  # type: ignore
 from pps_mw_training.utils.scaler import Scaler
+from pps_mw_training.utils.calculate_mean_std import get_std_mean
 
 
 def _load_data(
@@ -18,28 +15,25 @@ def _load_data(
     input_params: str,
     fill_value_input: float,
     fill_value_label: float,
+    training_label_name: str,
+    training_label_max: float,
+    training_label_min: float,
 ) -> list[np.ndarray]:
     """Load, scale, and filter data."""
 
-    with  xr.open_mfdataset(
+    with xr.open_mfdataset(
         [f.decode("utf-8") for f in data_files],
-        #[f for f in data_files],
         combine="nested",
         concat_dim="nscene",
     ) as all_data:
-
         all_data = all_data.sel(
             {
-                "npix": all_data["npix"].values[8:-8],
-                "nscan": all_data["nscan"].values[8:-8],
+                "npix": all_data["npix"].values,
+                "nscan": all_data["nscan"].values,
             }
         ).load()
-
-
         input_params = json.loads(input_params)
-
         scaler = Scaler.from_dict(input_params)
-
         input_data = np.stack(
             [
                 scaler.apply(all_data[p["name"]][:, :, :].values, idx)
@@ -47,30 +41,41 @@ def _load_data(
             ],
             axis=3,
         )
-
-        label_data = all_data["cloud_base"][:, :, :].values
-        #label_data = all_data["cloud_top"][:, :, :].values
+        label_data = all_data[training_label_name.decode("utf-8")][
+            :, :, :
+        ].values
         input_data[~np.isfinite(input_data)] = fill_value_input
         label_data[~np.isfinite(label_data)] = fill_value_label
-        label_data[label_data < 0] = fill_value_label
-        label_data[label_data >= 12000] = 12000.0
-
-        label_data[label_data > 0]  = (label_data[label_data > 0] - 0.)/(12000. - 0.)
+        label_data[label_data < training_label_min] = fill_value_label
+        label_data[label_data >= training_label_max] = training_label_max
+        label_data = (label_data - training_label_min) / (
+            training_label_max - training_label_min
+        )
+        # extra augmentation as rotate
         input_data, label_data = rotate_data(input_data, label_data)
+        return [
+            np.float32(input_data),
+            np.float32(np.expand_dims(label_data, axis=3)),
+        ]
 
-        return [np.float32(input_data), np.float32(np.expand_dims(label_data, axis=3))]
 
-
-def rotate_data(xtrain, ytrain):
+def rotate_data(
+    xtrain: np.ndarray, ytrain: np.ndarray
+) -> Tuple[np.ndarray, np.ndarray]:
+    """rotate training data and label data
+    should be in augmentation
+    """
     xrotated = np.empty(xtrain.shape)
     yrotated = np.empty(ytrain.shape)
     batchsize = xtrain.shape[0]
     nchannels = xtrain.shape[-1]
     for i in range(batchsize):
-        angle = random.choice([0., 90., 180., 270.])
+        angle = random.choice([0.0, 90.0, 180.0, 270.0])
         yrotated[i, :, :] = rotate(ytrain[i, :, :], angle, reshape=False)
         for j in range(nchannels):
-            xrotated[i, :, :, j] = rotate(xtrain[i, :, :, j], angle, reshape=False)
+            xrotated[i, :, :, j] = rotate(
+                xtrain[i, :, :, j], angle, reshape=False
+            )
     return xrotated, yrotated
 
 
@@ -80,13 +85,32 @@ def rotate_data(xtrain, ytrain):
         tf.TensorSpec(shape=(), dtype=tf.string),
         tf.TensorSpec(shape=(), dtype=tf.float32),
         tf.TensorSpec(shape=(), dtype=tf.float32),
+        tf.TensorSpec(shape=(), dtype=tf.string),
+        tf.TensorSpec(shape=(), dtype=tf.float32),
+        tf.TensorSpec(shape=(), dtype=tf.float32),
     )
 )
-def load_data(matched_files, input_params, fill_value_input, fill_value_label):
+def load_data(
+    matched_files,
+    input_params,
+    fill_value_input,
+    fill_value_label,
+    training_label_name,
+    training_label_max,
+    training_label_min,
+):
     """Load netcdf dataset."""
     return tf.numpy_function(
         func=_load_data,
-        inp=[matched_files, input_params, fill_value_input, fill_value_label],
+        inp=[
+            matched_files,
+            input_params,
+            fill_value_input,
+            fill_value_label,
+            training_label_name,
+            training_label_max,
+            training_label_min,
+        ],
         Tout=[tf.float32, tf.float32],
     )
 
@@ -97,57 +121,29 @@ def _get_training_dataset(
     input_params: str,
     fill_value_input: float,
     fill_value_label: float,
+    training_label_name: str,
+    training_label_max: float,
+    training_label_min: float,
 ) -> list[tf.data.Dataset]:
     """Get training dataset."""
 
-    print(matched_files)
-    ds = tf.data.Dataset.from_tensor_slices([f.as_posix() for f in matched_files])
+    ds = tf.data.Dataset.from_tensor_slices(
+        [f.as_posix() for f in matched_files]
+    )
     matched_files = [f.as_posix() for f in matched_files]
     ds = ds.batch(batch_size)
-
     ds = ds.map(
         lambda x: load_data(
             x,
             tf.constant(input_params),
             tf.constant(fill_value_input),
             tf.constant(fill_value_label),
+            tf.constant(training_label_name),
+            tf.constant(training_label_max),
+            tf.constant(training_label_min),
         ),
     )
     return ds
-
-
-def update_std_mean(
-    input_files: list[Path], input_params: list[dict[str, Any]]
-) -> list[dict[str, Any]]:
-    """calculate std and mean for the input dataset to normalise"""
-
-    all_dict = {}
-    with xr.open_dataset(input_files[0]) as ds:
-        for key in ds.keys():
-            all_dict[key] = {"n": 0, "x": 0, "x2": 0}
-
-    for file in input_files:
-        with xr.open_dataset(file) as ds:
-            ds = ds.sel(
-                {
-                    "npix": ds["nscan"].values[8:-8],
-                    "nscan": ds["npix"].values[8:-8],
-                }
-            )
-            for key in ds.keys():
-                all_dict[key]["n"] += np.sum((np.isfinite(ds[key].values)))
-                all_dict[key]["x"] += np.nansum(ds[key].values)
-                all_dict[key]["x2"] += np.nansum(ds[key].values ** 2)
-
-    for p in input_params:
-        key = p["name"]
-        p["std"] = np.sqrt(
-            (all_dict[key]["x2"] / all_dict[key]["n"])
-            - (all_dict[key]["x"] / all_dict[key]["n"]) ** 2
-        )
-        p["mean"] = all_dict[key]["x"] / all_dict[key]["n"]
-
-    return input_params
 
 
 def get_training_dataset(
@@ -157,49 +153,41 @@ def get_training_dataset(
     test_fraction: float,
     batch_size: int,
     input_params: list[dict[str, Any]],
+    training_label_name: str,
+    training_label_max: float,
+    training_label_min: float,
     fill_value_input: float,
     fill_value_label: float,
+    update_std_mean: bool,
 ) -> list[tf.data.Dataset]:
     """Get training dataset."""
-    print(training_data_path)
-    # assert train_fraction + validation_fraction + test_fraction == 1
-    input_files = list((training_data_path).glob("cnn_data*.nc*"))[:22000]
+
+    assert train_fraction + validation_fraction + test_fraction == 1
+    input_files = list((training_data_path).glob("cnn_data*.nc*"))[:100]
 
     s = len(input_files)
     train_size = int(s * train_fraction)
     validation_size = int(s * validation_fraction)
-    # input_params = update_std_mean(
-    #     input_files[0 : train_size + validation_size], input_params
-    # )
-    with open("/home/sm_indka/pps-mw-training/scripts/input_params.pickle", "rb") as f:
-        input_params_all = pickle.load(f)
 
-    # Create a dictionary from list2 for easy lookup by name
-    dict2 = {item['name']: item for item in input_params_all}
-
-    # Create the new list by merging the relevant fields
-    new_list = []
-    for item in input_params:
-        name = item['name']
-        if name in dict2:
-            # Create a new dictionary combining fields from list1 and std, mean from list2
-            new_item = {
-                "name": item["name"],
-                "scale": item["scale"],
-                "min": item["min"],
-                "max": item["max"],
-                "zscore_normalise": item["zscore_normalise"],
-                "std": dict2[name]["std"],
-                "mean": dict2[name]["mean"]
-            }
-            new_list.append(new_item)
-
-    params = json.dumps(new_list)
+    if update_std_mean:
+        input_params = get_std_mean(
+            input_files[0 : train_size + validation_size], input_params
+        )
+    params = json.dumps(input_params)
     return [
-        _get_training_dataset(f, batch_size, params, fill_value_input, fill_value_label)
+        _get_training_dataset(
+            f,
+            batch_size,
+            params,
+            fill_value_input,
+            fill_value_label,
+            training_label_name,
+            training_label_max,
+            training_label_min,
+        )
         for f in [
             input_files[0:train_size],
             input_files[train_size : train_size + validation_size],
-            input_files[train_size + validation_size : :],
+            input_files[train_size + validation_size :],
         ]
     ]
