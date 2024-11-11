35f0b4931c5a0a1dcd06f21ffd867fc7334fe506
sm_indka <inderpreet.kaur@smhi.se>
2024-11-11 15:30:41 +0100

    cleaned script to run training as bash script on Freja

--- scripts/run_batch.sh
+++ scripts/run_batch.sh
@@ -1,13 +1,10 @@
 #!/bin/bash -l                                                                                                    
 #SBATCH -A safnwc
-#SBATCH -n 3 # lower case n!
-#SBATCH -t 24:65:00 #Maximum time of 7:65 minutes
+#SBATCH -n 8 # lower case n!
+#SBATCH -t 4:00:00 #Maximum time of 4:00 hrs
 
-module load Anaconda/2023.09-0-hpc1
+module load Mambaforge/23.3.1-1-hpc1 
 conda activate pps-mw-training
 
 
 python train.py cloud_base  
-#python make_training_data_pixel_based_old_db.py
-#python split_files.py
-#python mean_std.py
