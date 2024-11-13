ae0fbde48e2b77d2cc3d373f85c8509c976d5bd5
sm_indka <inderpreet.kaur@smhi.se>
2024-11-13 22:36:33 +0100

    change scaler use, allow selection of label variable

--- pps_mw_training/pipelines/cloud_base/training_data.py
+++ pps_mw_training/pipelines/cloud_base/training_data.py
@@ -1,88 +1,57 @@
 from pathlib import Path
-from typing import Any, Tuple
+from typing import Any
 import json
-import random
 import numpy as np  # type: ignore
 import tensorflow as tf  # type: ignore
 import xarray as xr  # type: ignore
-from scipy.ndimage import rotate  # type: ignore
-from pps_mw_training.utils.scaler import Scaler, LabelScaler
+from pps_mw_training.utils.scaler import get_scaler
 
 
 def _load_data(
     data_files: np.ndarray,
-    input_params: str,
+    input_parameters: str,
+    label_parameters: str,
     fill_value_input: float,
     fill_value_label: float,
-    training_label_name: bytes,
-    training_label_max: float,
-    training_label_min: float,
 ) -> list[np.ndarray]:
     """Load, scale, and filter data."""
-
     with xr.open_mfdataset(
         [f.decode("utf-8") for f in data_files],
         combine="nested",
         concat_dim="nscene",
         engine="h5netcdf",
     ) as all_data:
-        all_data = all_data.sel(
-            {
-                "npix": all_data["npix"].values,
-                "nscan": all_data["nscan"].values,
-            }
-        ).load()
-        params = json.loads(input_params)
-        scaler = Scaler.from_dict(params)
+        all_data = all_data.load()
+        input_params = json.loads(input_parameters)
+        label_params = json.loads(label_parameters)
+        input_scaler = get_scaler(input_params)
         input_data = np.stack(
             [
-                scaler.apply(all_data[p["name"]][:, :, :].values, idx)
-                for idx, p in enumerate(params)
+                input_scaler.apply(all_data[p["name"]].values, idx)
+                for idx, p in enumerate(input_params)
             ],
             axis=3,
         )
-        label_data = all_data[training_label_name.decode("utf-8")].values
+        label_scaler = get_scaler(label_params)
+        label_data = np.stack(
+            [
+                label_scaler.apply(all_data[p["name"]].values, idx)
+                for idx, p in enumerate(label_params)
+            ],
+            axis=3,
+        )
         input_data[~np.isfinite(input_data)] = fill_value_input
         label_data[~np.isfinite(label_data)] = fill_value_label
-
-        label_scaler = LabelScaler(training_label_min, training_label_max)
-        label_data = label_scaler.scale(label_data, fill_value_label)
-
-        # extra augmentation as rotate
-        # input_data, label_data = rotate_data(input_data, label_data)
         return [
             np.float32(input_data),
-            np.float32(np.expand_dims(label_data, axis=3)),
+            np.float32(label_data),
         ]
-
-
-def rotate_data(
-    xtrain: np.ndarray, ytrain: np.ndarray
-) -> Tuple[np.ndarray, np.ndarray]:
-    """
-    rotate training data and label data
-    should be in augmentation
-    """
-    xrotated = np.empty(xtrain.shape)
-    yrotated = np.empty(ytrain.shape)
-    batchsize = xtrain.shape[0]
-    nchannels = xtrain.shape[-1]
-    for i in range(batchsize):
-        angle = random.choice([0.0, 90.0, 180.0, 270.0])
-        yrotated[i, :, :] = rotate(ytrain[i, :, :], angle, reshape=False)
-        for j in range(nchannels):
-            xrotated[i, :, :, j] = rotate(
-                xtrain[i, :, :, j], angle, reshape=False
-            )
-    return xrotated, yrotated
 
 
 @tf.function(
     input_signature=(
         tf.TensorSpec(shape=(None,), dtype=tf.string),
         tf.TensorSpec(shape=(), dtype=tf.string),
-        tf.TensorSpec(shape=(), dtype=tf.float32),
-        tf.TensorSpec(shape=(), dtype=tf.float32),
         tf.TensorSpec(shape=(), dtype=tf.string),
         tf.TensorSpec(shape=(), dtype=tf.float32),
         tf.TensorSpec(shape=(), dtype=tf.float32),
@@ -91,11 +60,9 @@ def rotate_data(
 def load_data(
     matched_files,
     input_params,
+    label_params,
     fill_value_input,
     fill_value_label,
-    training_label_name,
-    training_label_max,
-    training_label_min,
 ):
     """Load netcdf dataset."""
     return tf.numpy_function(
@@ -103,11 +70,9 @@ def load_data(
         inp=[
             matched_files,
             input_params,
+            label_params,
             fill_value_input,
             fill_value_label,
-            training_label_name,
-            training_label_max,
-            training_label_min,
         ],
         Tout=[tf.float32, tf.float32],
     )
@@ -117,11 +82,9 @@ def _get_training_dataset(
     matched_files: list[Path],
     batch_size: int,
     input_params: str,
+    label_params: str,
     fill_value_input: float,
     fill_value_label: float,
-    training_label_name: str,
-    training_label_max: float,
-    training_label_min: float,
 ) -> list[tf.data.Dataset]:
     """Get training dataset."""
 
@@ -133,11 +96,9 @@ def _get_training_dataset(
         lambda x: load_data(
             x,
             tf.constant(input_params),
+            tf.constant(label_params),
             tf.constant(fill_value_input),
             tf.constant(fill_value_label),
-            tf.constant(training_label_name),
-            tf.constant(training_label_max),
-            tf.constant(training_label_min),
         ),
         num_parallel_calls=1,
     )
@@ -150,10 +111,8 @@ def get_training_dataset(
     validation_fraction: float,
     test_fraction: float,
     batch_size: int,
-    input_params: list[dict[str, Any]],
-    training_label_name: str,
-    training_label_max: float,
-    training_label_min: float,
+    input_parameters: list[dict[str, Any]],
+    label_parameters: list[dict[str, Any]],
     fill_value_input: float,
     fill_value_label: float,
 ) -> list[tf.data.Dataset]:
@@ -166,21 +125,20 @@ def get_training_dataset(
     train_size = int(s * train_fraction)
     validation_size = int(s * validation_fraction)
 
-    params = json.dumps(input_params)
+    input_params = json.dumps(input_parameters)
+    label_params = json.dumps(label_parameters)
     return [
         _get_training_dataset(
             f,
             batch_size,
-            params,
+            input_params,
+            label_params,
             fill_value_input,
             fill_value_label,
-            training_label_name,
-            training_label_max,
-            training_label_min,
         )
         for f in [
             input_files[0:train_size],
-            input_files[train_size : train_size + validation_size],
-            input_files[train_size + validation_size :],
+            input_files[train_size: train_size + validation_size],
+            input_files[train_size + validation_size:],
         ]
     ]
