e88331090eeb3f44cfc39b72c9a26f4bfeb008e6
sm_indka <inderpreet.kaur@smhi.se>
2024-11-19 11:33:06 +0100

    split main function to two for easy readability

--- pps_mw_training/utils/calculate_mean_std.py
+++ pps_mw_training/utils/calculate_mean_std.py
@@ -11,12 +11,31 @@ from pps_mw_training.pipelines.cloud_base.settings import INPUT_PARAMS
 def get_std_mean(
     input_files: list[Path], input_params: list[dict[str, Any]]
 ) -> list[dict[str, Any]]:
-    """calculate std and mean for the input dataset to normalise"""
+    """
+    Calculate standard deviation and mean for the input dataset to normalize.
+    """
+    stats = get_stats(input_files, input_params)
 
+    for p in input_params:
+        parameter = p["name"]
+        mean, std = calculate_mean_std(stats[parameter])
+        p["mean"] = mean
+        p["std"] = std
+
+    return input_params
+
+
+def get_stats(
+    input_files: list[Path], input_params: list[dict[str, Any]]
+) -> list[dict[str, Any]]:
+    """
+    Calculate intermediate statistics to calculate mean and std
+    """
     stats = {}
     for idx, file in enumerate(input_files):
         with xr.open_dataset(file.as_posix()) as ds:
-            for parameter in ds:
+            for p in input_params:
+                parameter = p["name"]
                 data = ds[parameter].values
                 if idx == 0:
                     stats[parameter] = {
@@ -28,16 +47,16 @@ def get_std_mean(
                 stats[parameter]["n"] += np.count_nonzero(np.isfinite(data))
                 stats[parameter]["sum"] += np.nansum(data)
                 stats[parameter]["sum_of_squares"] += np.nansum(data**2)
+    return stats
 
-    for p in input_params:
-        parameter = p["name"]
-        p["mean"] = stats[parameter]["sum"] / stats[parameter]["n"]
-        p["std"] = np.sqrt(
-            (stats[parameter]["sum_of_squares"] / stats[parameter]["n"])
-            - (stats[parameter]["sum"] / stats[parameter]["n"]) ** 2
-        )
 
-    return input_params
+def calculate_mean_std(stats: dict[str, float]) -> tuple[float, float]:
+    """
+    Calculate mean and standard deviation from intermediate stats
+    """
+    mean = stats["sum"] / stats["n"]
+    std = np.sqrt((stats["sum_of_squares"] / stats["n"]) - (mean**2))
+    return mean, std
 
 
 def main():
@@ -51,7 +70,7 @@ def main():
     )
     args = parser.parse_args()
     input_dir = Path(args.input_dir)
-    input_files = list((input_dir).glob("cnn_data*.nc*"))
+    input_files = list((input_dir).glob("cnn_data*.nc*"))[:100]
     input_params = get_std_mean(input_files, INPUT_PARAMS)
     with open("input_params.json", "w") as outfile:
         outfile.write(json.dumps(input_params, indent=4))
