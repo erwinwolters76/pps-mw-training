61d0d03f80d85385a336d7f8d4a71cee05fb0833
sm_indka <inderpreet.kaur@smhi.se>
2024-11-20 11:47:29 +0100

    refactoring for easy readability

--- pps_mw_training/utils/calculate_mean_std.py
+++ pps_mw_training/utils/calculate_mean_std.py
@@ -1,80 +1,51 @@
-import argparse
 from typing import Any
 from pathlib import Path
-import json
 
 import numpy as np  # type: ignore
 import xarray as xr  # type: ignore
-from pps_mw_training.pipelines.cloud_base.settings import INPUT_PARAMS
+
+
+def get_stats(
+    input_files: list[Path], input_params: list[dict[str, Any]]
+) -> list[dict[str, Any]]:
+    """
+    Calculate standard deviation and mean for the input dataset to normalize.
+    """
+    return get_std_mean(input_files, input_params)
 
 
 def get_std_mean(
     input_files: list[Path], input_params: list[dict[str, Any]]
 ) -> list[dict[str, Any]]:
     """
-    Calculate standard deviation and mean for the input dataset to normalize.
-    """
-    stats = get_stats(input_files, input_params)
-
-    for p in input_params:
-        parameter = p["name"]
-        mean, std = calculate_mean_std(stats[parameter])
-        p["mean"] = mean
-        p["std"] = std
-
-    return input_params
-
-
-def get_stats(
-    input_files: list[Path], input_params: list[dict[str, Any]]
-) -> dict[str, Any]:
-    """
     Calculate intermediate statistics to calculate mean and std
+    and then update the input_params dict
     """
-    stats = {}
-    for idx, file in enumerate(input_files):
-        with xr.open_dataset(file.as_posix()) as ds:
-            for p in input_params:
-                parameter = p["name"]
-                data = ds[parameter].values
-                if idx == 0:
-                    stats[parameter] = {
-                        "n": np.count_nonzero(np.isfinite(data)),
-                        "sum": np.nansum(data),
-                        "sum_of_squares": np.nansum(data**2),
-                    }
+
+    stats = {
+        p["name"]: {"n": 0, "sum": 0.0, "sum_of_squares": 0.0}
+        for p in input_params
+    }
+    for file in input_files:
+        with xr.open_dataset(file) as ds:
+            for parameter in ds:
+                if parameter not in stats:
                     continue
-                stats[parameter]["n"] += np.count_nonzero(np.isfinite(data))
+                data = ds[parameter].values
+                stats[parameter]["n"] += np.sum(np.isfinite(data))
                 stats[parameter]["sum"] += np.nansum(data)
                 stats[parameter]["sum_of_squares"] += np.nansum(data**2)
-    return stats
+    return [
+        param | calculate_std_mean(stats[param["name"]])
+        for param in input_params
+        if param["name"] in stats
+    ]
 
 
-def calculate_mean_std(stats: dict[str, float]) -> tuple[float, float]:
+def calculate_std_mean(stats: dict[str, float]) -> dict[str, float]:
     """
     Calculate mean and standard deviation from intermediate stats
     """
     mean = stats["sum"] / stats["n"]
     std = np.sqrt((stats["sum_of_squares"] / stats["n"]) - (mean**2))
-    return mean, std
-
-
-def main():
-    parser = argparse.ArgumentParser(
-        description="Calculate std and mean for input datasets."
-    )
-    parser.add_argument(
-        "input_dir",
-        type=str,
-        help="Path to the directory containing input dataset files.",
-    )
-    args = parser.parse_args()
-    input_dir = Path(args.input_dir)
-    input_files = list((input_dir).glob("cnn_data*.nc*"))
-    input_params = get_std_mean(input_files, INPUT_PARAMS)
-    with open("input_params.json", "w") as outfile:
-        outfile.write(json.dumps(input_params, indent=4))
-
-
-if __name__ == "__main__":
-    main()
+    return {"mean": mean, "std": std}
