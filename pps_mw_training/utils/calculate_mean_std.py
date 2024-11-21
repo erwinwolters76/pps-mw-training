0c82f33388351b44eff028ea80d2895472af1021
sm_indka <inderpreet.kaur@smhi.se>
2024-11-21 10:13:15 +0100

    refactoring

--- pps_mw_training/utils/calculate_mean_std.py
+++ pps_mw_training/utils/calculate_mean_std.py
@@ -6,36 +6,38 @@ import xarray as xr  # type: ignore
 
 
 def get_std_mean(
-    input_files: list[Path], input_params: list[dict[str, Any]]
-) -> list[dict[str, Any]]:
+    input_files: list[Path], params: list[str]
+) -> dict[str, dict[str, float]]:
     """
-    Get standard deviation and mean associated to given parameters of dataset
+    Get standard deviation and mean associated to given parameters of dataset.
     """
 
-    stats = {
-        p["name"]: {"n": 0, "sum": 0.0, "sum_of_squares": 0.0}
-        for p in input_params
-    }
+    def _get_std_mean(
+        n: int, sum: float, sum_of_squares: float
+    ) -> dict[str, float]:
+        """Get standard deviation and mean from intermediate stats."""
+        return {
+            "mean": sum / n,
+            "std": np.sqrt((sum_of_squares / n) - ((sum / n) ** 2)),
+        }
+
+    stats = {p: {"n": 0, "sum": 0.0, "sum_of_squares": 0.0} for p in params}
     for file in input_files:
         with xr.open_dataset(file) as ds:
-            for parameter in ds:
-                if parameter not in stats:
-                    continue
-                data = ds[parameter].values
-                stats[parameter]["n"] += np.sum(np.isfinite(data))
-                stats[parameter]["sum"] += np.nansum(data)
-                stats[parameter]["sum_of_squares"] += np.nansum(data**2)
-    return [
-        param | calculate_std_mean(stats[param["name"]])
-        for param in input_params
-        if param["name"] in stats
-    ]
+            for p in params:
+                stats[p]["n"] += np.count_nonzero(ds[p].values)
+                stats[p]["sum"] += np.nansum(ds[p].values)
+                stats[p]["sum_of_squares"] += np.nansum(ds[p].values ** 2)
+
+    return {
+        p: _get_std_mean(stat["n"], stat["sum"], stat["sum_of_squares"])
+        for p, stat in stats.items()
+    }
 
 
-def calculate_std_mean(stats: dict[str, float]) -> dict[str, float]:
-    """
-    Calculate mean and standard deviation from intermediate stats
-    """
-    mean = stats["sum"] / stats["n"]
-    std = np.sqrt((stats["sum_of_squares"] / stats["n"]) - (mean**2))
-    return {"mean": mean, "std": std}
+def update_params(
+    params: list[dict[str, Any]],
+    data: dict[str, dict[str, Any]],
+) -> list[dict[str, Any]]:
+    """Update parameters with desired data."""
+    return [p | data.get(p["name"], {}) for p in params]
